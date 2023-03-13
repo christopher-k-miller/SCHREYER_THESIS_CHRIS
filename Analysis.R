998f36ba3e4af6436e86175f87775b93468da255
Christopher Miller <christopherkmiller04@gmail.com>
2023-03-12 21:07:45 -0400

    3/12

--- Analysis.R
+++ Analysis.R
@@ -8,10 +8,36 @@ library(scales)
 
 #EDA
 
+regular1 <- tidy %>%
+  group_by(team1,date.x) %>%
+  filter(Season == 2011 & (conf == 1 | conf == 0) & team1 == "Penn St.") %>%
+  ungroup() %>%
+  select(t1adjo,t1adjd,date.x)
 
+regular2 <- tidy %>%
+  group_by(team2,date.x) %>%
+  filter(Season == 2011 & (conf == 1 | conf == 0) & team2 == "Penn St.") %>%
+  ungroup() %>%
+  select(t2adjo,t2adjd,date.x)
+regular1 <- rename(regular1, adjo = t1adjo,adjd = t1adjd)
+regular2 <- rename(regular2, adjo = t2adjo,adjd = t2adjd)
+regular3 <- bind_rows(regular1,regular2)
+regular3 <- regular3 %>%
+  arrange(date.x)
 
-#TS
+regular3$adj <- regular3$adjo - regular3$adjd
 
+myts <- ts(regular3$adj, frequency=1)
+MA <- auto.arima(myts,allowdrift = TRUE,allowmean = TRUE, seasonal = FALSE, trace = T)
+MA2 <- ets(myts, model = "ZZN")
+
+ts.plot(myts)
+myts %>% diff() %>% ggtsdisplay(main="")
+ts.plot(myts)
+points(MA$fitted, type = "l", col = 2, lty = 2)
+ts.plot(myts)
+points(MA2$fitted, type = "l", col = 2, lty = 2)
+summary(MA2)
 
 
 
@@ -23,10 +49,10 @@ probmethodtable <- as.data.frame(cbind(seed2011$team,seed2011$teamseed,round(see
 probmethodtable <- probmethodtable %>% 
   arrange(V2)
 probmethodtable <- probmethodtable[1:16,]
-names(probmethodtable) <- c('Team','Seed','Seed Method R2','Seed Method R3','ARIMA Method R2','ARIMA Method R3','ETS Method R2','ETS Method R3')
+names(probmethodtable) <- c('Team','Seed','Seed Method R1','Seed Method R2','ARIMA Method R1','ARIMA Method R2','ETS Method R1','ETS Method R2')
 probmethodtable$Seed <- parse_number(probmethodtable$Seed)
 probmethodtable %>%
-  kbl(caption = "Probabilites of Making Round 2 and 3 using Markov Chain") %>%
+  kbl(caption = "Probabilites of Making Round 1 and 2 using Markov Chain") %>%
   kable_classic(full_width = F, html_font = "Cambria") %>%
   column_spec(2, border_right = TRUE)%>%
   column_spec(4, border_right = TRUE)%>%
@@ -66,11 +92,11 @@ ggplot(seed, aes(x=teamseed, y=value, group=variable)) +
 pctresults <- compare_results %>%
   select(-c(tsnum,seednum,etsnum))
 pctresults <- pctresults[, c(1, 6, 2, 4, 7, 3, 5)]
-names(pctresults) <- c('Year','Seed Method R2 % Correct','ARIMA Method R2 % Correct','ETS Method R2 % Correct','Seed Method R3 % Correct','ARIMA Method R3 % Correct','ETS Method R3 % Correct')
+names(pctresults) <- c('Year','Seed Method R1 % Correct','ARIMA Method R1 % Correct','ETS Method R1 % Correct','Seed Method R2 % Correct','ARIMA Method R2 % Correct','ETS Method R2 % Correct')
 pctresults[2:7] <- sapply(pctresults[2:7], function(x) percent(x, accuracy=.01))
 
 pctresults %>%
-  kbl(caption = "Percent Correct in R2 and R3") %>%
+  kbl(caption = "Percent Correct in R1 and R2") %>%
   kable_classic(full_width = F, html_font = "Cambria") %>%
   column_spec(1, border_right = TRUE)%>%
   column_spec(4, border_right = TRUE)
@@ -285,35 +311,105 @@ mean(tsupsetcorrect/tsupset)
 mean(etsupsetcorrect/etsupset)
 
 
-# seed <- rbind(seed2011,seed2012,seed2013,seed2014,seed2015,seed2016,seed2017,seed2018,seed2019)
-# seed$teamseed <- parse_number(seed$teamseed)
-# seed <- seed %>%
-#   group_by(teamseed)%>%
-#   summarise('Seed Method R3' = mean(pr2))
-# ts <- rbind(ts2011,ts2012,ts2013,ts2014,ts2015,ts2016,ts2017,ts2018,ts2019)
-# ts$teamseed <- parse_number(ts$teamseed)
-# ts <- ts %>%
-#   group_by(teamseed)%>%
-#   summarise('ARIMA Method R3' = mean(pr2))
-# ets <- rbind(ets2011,ets2012,ets2013,ets2014,ets2015,ets2016,ets2017,ets2018,ets2019)
-# ets$teamseed <- parse_number(ets$teamseed)
-# ets <- ets %>%
-#   group_by(teamseed)%>%
-#   summarise('ETS Method R3' = mean(pr2))
-# seed <- left_join(seed,ts)
-# seed <- left_join(seed,ets)
-# seed$teamseed <- as.character(seed$teamseed)
-# 
-# seed$groups <- ifelse(as.numeric(seed$teamseed) <= 4, 1, ifelse(as.numeric(seed$teamseed) <=12,2,3))
-# seed$groups <- as.character(seed$groups)
-# names(seed) <- c('teamseed','a','b','c','groups')
-# seed <- seed %>%
-#   group_by(groups) %>%
-#   summarise(seedexpected = 36*sum(a),tsexpected = 36*sum(b),etsexpected = 36*sum(c))
-# 
-# chisquareobsevered <- as.data.frame(chisquareobsevered)
-# chisquareobsevered$groups <- ifelse(chisquareobsevered$chisquareobsevered <= 4, 1, ifelse(chisquareobsevered$chisquareobsevered <=12,2,3))
-# chisquareobsevered$groups <- as.character(chisquareobsevered$groups)
-# chisquareobsevered <- chisquareobsevered %>%  
-#   group_by(groups)%>%
-#   summarise(observed = n())
+
+k <- 2011
+y <- list()
+x <- list()
+n <- list()
+o <- list()
+while (k <= 2019){
+  sname <- paste("predicttsr1",as.character(k), sep = "")
+  dname <- get(paste("predicttsr1",as.character(k), sep = ""))
+  x[[sname]] <- dname
+  sname <- paste("predictetsr1",as.character(k), sep = "")
+  dname <- get(paste("predictetsr1",as.character(k), sep = ""))
+  y[[sname]] <- dname
+  k <- k+1
+}
+k <- 2011
+while (k <= 2019){
+  sname <- paste("predicttsr2",as.character(k), sep = "")
+  dname <- get(paste("predicttsr2",as.character(k), sep = ""))
+  n[[sname]] <- dname
+  sname <- paste("predictetsr2",as.character(k), sep = "")
+  dname <- get(paste("predictetsr2",as.character(k), sep = ""))
+  o[[sname]] <- dname
+  k <- k+1
+}
+
+
+tsupsetcorrectr1 <- {}
+etsupsetcorrectr1 <- {}
+tsupsetcorrectr2 <- {}
+etsupsetcorrectr2 <- {}
+for (i in x) {
+  i$upset <- ifelse(parse_number(i$WTeamSeed)>parse_number(i$LTeamSeed),1,0)
+  i$predupset <- ifelse((parse_number(i$WTeamSeed)>parse_number(i$LTeamSeed))&(i$pr1.x > i$pr1.y)|(parse_number(i$LTeamSeed)>parse_number(i$WTeamSeed))&(i$pr1.y > i$pr1.x) ,1 , 0)
+  i$upsetcorrect<- ifelse( i$upset == 1 & i$predupset == 1 ,1,0)
+  i$upsetincorrect <- ifelse( i$upset == 1 & i$predupset == 0,1,0)
+  
+  
+  sumupsetcorrect <- sum(i$predupset)
+  
+  
+  tsupsetcorrectr1 <-append(tsupsetcorrectr1,sumupsetcorrect)
+  
+}
+for (i in y) {
+  i$upset <- ifelse(parse_number(i$WTeamSeed)>parse_number(i$LTeamSeed),1,0)
+  i$predupset <- ifelse((parse_number(i$WTeamSeed)>parse_number(i$LTeamSeed))&(i$pr1.x > i$pr1.y)|(parse_number(i$LTeamSeed)>parse_number(i$WTeamSeed))&(i$pr1.y > i$pr1.x) ,1 , 0)
+  i$upsetcorrect<- ifelse( i$upset == 1 & i$predupset == 1 ,1,0)
+  i$upsetincorrect <- ifelse( i$upset == 1 & i$predupset == 0,1,0)
+  
+  
+  
+  sumupsetcorrect <- sum(i$predupset)
+  
+  etsupsetcorrectr1 <-append(etsupsetcorrectr1,sumupsetcorrect)
+}
+for (i in n) {
+  i$upset <- ifelse(parse_number(i$WTeamSeed)>parse_number(i$LTeamSeed),1,0)
+  i$predupset <- ifelse((parse_number(i$WTeamSeed)>parse_number(i$LTeamSeed))&(i$pr2.x > i$pr2.y)& (test$pr2.xind != 1)|(parse_number(i$LTeamSeed)>parse_number(i$WTeamSeed))&(i$pr2.y > i$pr2.x)& (test$pr2.yind != 1) ,1 , 0)
+  i$upsetcorrect<- ifelse( i$upset == 1 & i$predupset == 1 ,1,0)
+  i$upsetincorrect <- ifelse( i$upset == 1 & i$predupset == 0,1,0)
+  
+  
+  sumupsetcorrect <- sum(i$predupset)
+  
+  tsupsetcorrectr2 <-append(tsupsetcorrectr2,sumupsetcorrect)
+}
+for (i in o) {
+  i$upset <- ifelse(parse_number(i$WTeamSeed)>parse_number(i$LTeamSeed),1,0)
+  i$predupset <- ifelse((parse_number(i$WTeamSeed)>parse_number(i$LTeamSeed))&(i$pr2.x > i$pr2.y)& (test$pr2.xind != 1)|(parse_number(i$LTeamSeed)>parse_number(i$WTeamSeed))&(i$pr2.y > i$pr2.x)& (test$pr2.yind != 1) ,1 , 0)
+  i$upsetcorrect<- ifelse( i$upset == 1 & i$predupset == 1 ,1,0)
+  i$upsetincorrect <- ifelse( i$upset == 1 & i$predupset == 0,1,0)
+  
+  
+  sumupsetcorrect <- sum(i$predupset)
+  
+  etsupsetcorrectr2 <-append(etsupsetcorrectr2,sumupsetcorrect)
+  
+
+}
+
+tspredupset <- tsupsetcorrectr1 + tsupsetcorrectr2
+etspredupset <- etsupsetcorrectr1 + etsupsetcorrectr2
+
+
+upsetpct2 <- as.data.frame(yearw)
+upsetpct2 <- rename(upsetpct2,Year = yearw)
+upsetpct2$'ARIMA Method % Correct' <- percent(tsupsetcorrect/tspredupset,.01)
+upsetpct2$'ETS Method % Correct' <- percent(etsupsetcorrect/etspredupset,.01)
+
+upsetpct2 %>%
+  kbl(caption = "Percent of Predicted Upsets Correct") %>%
+  kable_classic(full_width = F, html_font = "Cambria") %>%
+  column_spec(1, border_right = TRUE)
+
+mean(tsupsetcorrect/tspredupset)
+mean(etsupsetcorrect/etspredupset)
+sd(tsupsetcorrect/tspredupset)
+sd(etsupsetcorrect/etspredupset)
+mean(tspredupset)
+mean(etspredupset)
+
